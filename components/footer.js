3172aeeb587af8b3ca606cd14f9559b2dcf6e605
Ryan Rambali <ryanrambali@gmail.com>
2024-05-02 20:48:32 -0400

    fixed contact icons

--- components/footer.js
+++ components/footer.js
@@ -10,14 +10,16 @@ class Footer extends HTMLElement {
     <footer class="footer">
       <div class="content">
         <section class="section">
+        <div class="container">
           <h2>Get In Touch</h2>
           <div>
-          <a href="https://www.linkedin.com/in/ryan-rambali-0834061a2/"><span>Linkedin</span></a>
-          <a href="https://github.com/rambolps/"><span>Github</span></a>
-          <a href="mailto:ryan@rambolps.ca"><span>Mail</span></a>
+          <a href="https://www.linkedin.com/in/ryan-rambali-0834061a2/"><span class="ConIcons"><img src="/images/icons/linkedin (Custom).png" alt="linkedin"></span></a>
+          <a href="https://github.com/rambolps/"><span class="ConIcons"><img src="/images/icons/github (Custom).png" alt="GitHub"></span></a>
+          <a href="mailto:ryan@rambolps.ca"><span class="ConIcons"><img src="/images/icons/mail (Custom).png" alt="Mail"></span></a>
           </div>
           <br>
           <p>&copy; Ryan Rambali</p>
+          </div>
         </section>
       </div>
     </footer>
